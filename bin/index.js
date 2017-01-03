a6f883a89bd49864310e28e0e649b0b0bde1dcc6
Haseeb Qureshi <haseeb.n.qureshi@gmail.com>
2017-01-03 14:09:43 -0500

    completely refactoring entire service, have basic prompts

--- bin/index.js
+++ bin/index.js
@@ -1,42 +1,126 @@
 #! /usr/bin/env node
 
-var cmd = process.argv[2] || 'undefined';
-
-var fs = require('fs');
-
-var filepath = `${__dirname}/${cmd}.js`;
+var _ = require('underscore');
 
 var chalk = require('chalk');
 
-//if there's no sls-hq command, automatically show help
-if (process.argv.length < 3) {
-	filepath = `${__dirname}/help.js`;
-}
+var inquirer = require('inquirer');
 
-//overriding our filecheck logic with our sls-hq command help
-else if (process.argv[3] === 'help') {
-	filepath = `${__dirname}/help.js`;
-}
+console.log(
+	  `\n` + chalk.gray(`| `) + chalk.yellow.bold(`SERVERLESS HQ`)
+	+ `\n` + chalk.gray(`| `) + chalk.cyan.bold(`Serverless made quick & easy!`)
+	+ `\n` + chalk.gray(`| `) + `This is your home base for quick & powerfully easy scaffolding`
+	+ `\n` + chalk.gray(`| `) + `for your next Serverless project.`
+	+ `\n` + chalk.gray(`| `) + chalk.gray(`Version 0.3.0`)
+	+ `\n` + chalk.gray(`| `)
+	+ `\n` + chalk.gray(`| `) + chalk.white(`crafted by hq (2016-2017)`)
+	+ `\n` + chalk.gray(`| `) + chalk.gray(`twitter.com/_hq, github.com/haseebnqureshi`)
+	+ `\n` + chalk.gray(`| `) + chalk.gray(`made in knoxville, tennessee`)
+	+ `\n` + chalk.gray(`| `)
+	+ `\n` + chalk.gray(`| `) + `Let's get started!`
+	+ `\n` + chalk.gray(`| `) + chalk.gray(`Answer the prompts and in seconds, you'll be up and running.`)
+	+ `\n`
+);
 
-//try and see if requested command exists
-try {
-	fs.accessSync(filepath);
-}
+var questions = [
+	{ 
+		name: "SLS_HQ_service",
+		type: "input",
+		message: chalk.cyan("Project Name:"),
+		default: "serverless-hq",
+		filter: function(str) {
+			return str.toLowerCase();
+		}
+	},
+	{ 
+		name: "SLS_HQ_providerName",
+		type: "input",
+		message: chalk.yellow("  Provider:"),
+		default: "aws"
+	},
+	{ 
+		name: "SLS_HQ_providerRuntime",
+		type: "input",
+		message: chalk.yellow("  Runtime:"),
+		default: "nodejs4.3"
+	},
+	{ 
+		name: "SLS_HQ_providerProfile",
+		type: "input",
+		message: chalk.yellow("  AWS Profile:"),
+		default: "default"
+	},
+	{ 
+		name: "SLS_HQ_providerRegion",
+		type: "input",
+		message: chalk.yellow("  AWS Region:"),
+		default: "us-east-1"
+	},
+	{ 
+		name: "SLS_HQ_providerStage",
+		type: "input",
+		message: chalk.yellow("  AWS Stage:"),
+		default: "dev"
+	},
+	{
+		name: "createApi",
+		type: "confirm",
+		message: chalk.cyan("Scaffold API?"),
+		default: true
+	},
+	{
+		name: "apiResources",
+		type: "input",
+		message: chalk.yellow("  CRUD Resources (comma separated):"),
+		default: 'Items, Lists, Users',
+		filter: function(str) {
+			return str;
+		},
+		when: function(answers) {
+			return answers.createApi === true;
+		}
+	},
+	{
+		name: "createHtml",
+		type: "confirm",
+		message: chalk.cyan("Scaffold HTML Website?"),
+		default: true
+	},
+	{
+		name: "htmlBucketName",
+		type: "input",
+		message: chalk.yellow("  S3 BucketName:"),
+		default: 'serverless-hq.com',
+		filter: function(str) {
+			return str.toLowerCase();
+		},
+		when: function(answers) {
+			return answers.createHtml === true;
+		}
+	},
+	{
+		name: "htmlForceNonWww",
+		type: "confirm",
+		message: function(answers) {
+			return chalk.yellow(`  Redirect www.${answers.htmlBucketName} to ${answers.htmlBucketName}?`);
+		},
+		default: true,
+		when: function(answers) {
+			return answers.html === true;
+		}
+	},
 
-//otherwise, throw an error and alert our user
-catch (err) {
-	console.log(chalk.red(`! sls-hq: Couldn't find command "${cmd}"...`));
-	console.log(chalk.yellow(`! sls-hq: Please refer to "sls-hq help" or "sls-hq" for a list of valid commands...`));
-	return;
-}
+];
 
-//finally, try and load our command module
-try {
-	require(filepath)(process.argv);
-}
+inquirer.prompt(questions).then((answers) => {
 
-//and throw an error if something goes wrong
-catch (err) {
-	console.log(chalk.red('! sls-hq: Something went wrong...'));
-	console.log(chalk.red(err));
-}
+	console.log(
+		  `\n` + chalk.gray(`| `)
+		+ `\n` + chalk.gray(`| `) + chalk.green.bold(`Okay!`)
+		+ `\n` + chalk.gray(`| `) + chalk.gray.bold(`Scaffolding your project now...`)
+		+ `\n` + chalk.gray(`| `)
+	);
+
+	console.log(answers);
+
+});
